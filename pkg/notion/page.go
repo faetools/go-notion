6f839221683c95b77f79344b37f6707a06406dda
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2022-07-04 21:29:56 +0200

    get page title

---
+++ pkg/notion/page.go
@@ -0,0 +1,17 @@
+package notion
+
+// Title returns the page title.
+func (p Page) Title() string {
+	return p.Properties.title()
+}
+
+// Title returns the title of the page.
+func (props PropertyValueMap) title() string {
+	for _, prop := range props {
+		if prop.Title != nil {
+			return prop.Title.rawContent()
+		}
+	}
+
+	return ""
+}
