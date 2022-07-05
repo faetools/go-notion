9c072045ad5ddb07a66e56a1f2b1216ccfd9fb0c
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2022-07-05 18:54:13 +0200

    add (PropertyOptions).GetNames()

--- pkg/notion/util.go
+++ pkg/notion/util.go
@@ -3,7 +3,6 @@ package notion
 // TitleProperty represents a Title property.
 var TitleProperty = PropertyMeta{
 	Id:    "title", // must be this
-	Name:  "Title",
 	Type:  PropertyTypeTitle,
 	Title: &map[string]interface{}{},
 }
@@ -21,4 +20,15 @@ func NewRichText(content string) RichText {
 		Text:        &Text{Content: content},
 		Annotations: Annotations{Color: ColorDefault},
 	}
+}
+
+// GetNames returns names of all selected options.
+func (opts PropertyOptions) GetNames() []string {
+	names := make([]string, len(opts))
+
+	for i, sel := range opts {
+		names[i] = sel.Name
+	}
+
+	return names
 }
