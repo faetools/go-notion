ffa652b427ac53278144b99bb148f2961bd8bc34
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2022-07-09 13:27:38 +0200

    Release v0.0.16

--- notion/version.gen.go
+++ notion/version.gen.go
@@ -6,7 +6,7 @@ import "github.com/MarkRosemaker/semver"
 
 var (
 	// Version is the version of go-notion.
-	Version = semver.MustParse("0.0.15")
+	Version = semver.MustParse("0.0.16")
 
 	// GoVersion declares what version of Go should be used.
 	GoVersion = semver.MustParse("1.18.3")
