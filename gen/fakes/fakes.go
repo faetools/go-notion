57c970fbe286ef171b35e76245f75347ccff8281
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2022-07-15 08:33:53 +0200

    implement fake package

--- gen/fakes/fakes.go
+++ gen/fakes/fakes.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 
@@ -14,39 +17,92 @@ import (
 func main() {
 	ctx := context.Background()
 
-	g := cgtools.NewOsGenerator()
-
-	cl, err := notion.NewDefaultClient(os.Getenv("NOTION_TOKEN"))
+	cli, err := notion.NewDefaultClient(os.Getenv("NOTION_TOKEN"))
 	checkErr(err)
 
-	for _, v := range []struct {
-		getContent func() []byte
-		fileName   string
-	}{
-		{func() []byte {
-			resp, err := cl.GetPage(ctx, fake.PageID)
-			checkErr(err)
-
-			return resp.Body
-		}, "get-page.json"},
-		{func() []byte {
-			resp, err := cl.GetBlocks(ctx, fake.PageID, &notion.GetBlocksParams{})
-			checkErr(err)
-
-			return resp.Body
-		}, "get-blocks.json"},
-	} {
-		path := filepath.Join("../pkg/fake/", v.fileName)
-
-		if err := g.WriteBytes(path, v.getContent()); err != nil {
-			log.Fatal(err)
-		}
-
+	g := &fakesGenerator{
+		Generator: cgtools.NewOsGenerator(),
+		cli:       cli,
 	}
+
+	g.genGetPageResponse(ctx, fake.PageID)
+	g.genGetBlocksResponse(ctx, fake.PageID)
 }
 
 func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type fakesGenerator struct {
+	*cgtools.Generator
+	cli *notion.Client
+}
+
+func (g *fakesGenerator) genResponseFactory(
+	urlPathFormat string,
+	getResponse func(context.Context, notion.Id) (*http.Response, []byte),
+	followUp func(context.Context, []byte),
+) func(context.Context, notion.Id) {
+	return func(ctx context.Context, id notion.Id) {
+		urlPath := fmt.Sprintf(urlPathFormat, id)
+		filePath := filepath.Join("../pkg/fake/", urlPath+".json")
+
+		content, err := os.ReadFile(filePath)
+		if err != nil {
+			resp, body := getResponse(ctx, id)
+
+			if want := resp.Request.URL.Path; want != urlPath {
+				log.Fatalf("mismatched paths: %q vs %q", want, urlPath)
+			}
+
+			if resp.StatusCode != http.StatusOK {
+				log.Fatalf("status for %q not OK but %s", urlPath, resp.Status)
+			}
+
+			content = body
+
+			checkErr(g.WriteBytes(filePath, content))
+		}
+
+		if followUp != nil {
+			followUp(ctx, content)
+		}
+	}
+}
+
+func (g *fakesGenerator) genGetPageResponse(ctx context.Context, id notion.Id) {
+	g.genResponseFactory(
+		"/v1/pages/%s",
+		func(ctx context.Context, id notion.Id) (*http.Response, []byte) {
+			resp, err := g.cli.GetPage(ctx, id)
+			checkErr(err)
+
+			return resp.HTTPResponse, resp.Body
+		},
+		nil,
+	)(ctx, id)
+}
+
+func (g *fakesGenerator) genGetBlocksResponse(ctx context.Context, id notion.Id) {
+	g.genResponseFactory(
+		"/v1/blocks/%s/children",
+		func(ctx context.Context, id notion.Id) (*http.Response, []byte) {
+			resp, err := g.cli.GetBlocks(ctx, id, &notion.GetBlocksParams{})
+			checkErr(err)
+
+			return resp.HTTPResponse, resp.Body
+		},
+		func(ctx context.Context, b []byte) {
+			var blockList notion.BlocksList
+			checkErr(json.Unmarshal(b, &blockList))
+
+			for _, block := range blockList.Results {
+				if block.HasChildren {
+					g.genGetBlocksResponse(ctx, notion.Id(block.Id))
+				}
+			}
+		},
+	)(ctx, id)
 }
--- pkg/fake/fake.go
+++ pkg/fake/fake.go
@@ -1,20 +1,40 @@
 package fake
 
 import (
-	_ "embed" // fake responses
+	"embed" // fake responses
+	"net/http"
+	"testing"
 
 	"github.com/faetools/go-notion/pkg/notion"
+	"github.com/kjk/common/require"
+	"gopkg.in/h2non/gock.v1"
 )
 
 // PageID is the page ID of our example page.
 const PageID notion.Id = "96245c8f178444a482ad1941127c3ec3"
 
-// GetPageResponse is the response we got by calling GetPage on the example page.
+// Responses contains a number of responses we have generated.
 //
-//go:embed get-page.json
-var GetPageResponse string
+//go:embed v1
+var Responses embed.FS
 
-// GetBlocksResponse is the response we got by calling GetBlocks on the example page.
-//
-//go:embed get-blocks.json
-var GetBlocksResponse string
+// ResponseTo returns the body of a fake response to a GET request for the stated path.
+func ResponseTo(t *testing.T, path string) []byte {
+	t.Helper()
+
+	body, err := Responses.ReadFile(path + ".json")
+	require.NoError(t, err)
+
+	return body
+}
+
+// MockResponseTo mocks a response to a particular path.
+func MockResponseTo(t *testing.T, path string) {
+	t.Helper()
+
+	gock.New("https://api.notion.com").
+		Path(path).
+		Reply(http.StatusOK).
+		SetHeader("Content-Type", "json").
+		BodyString(string(ResponseTo(t, path)))
+}
