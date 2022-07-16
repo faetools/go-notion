bc401a5b897d9b45ff4e1fcdbe9049b04209d0d5
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2022-07-17 00:32:52 +0200

    fix PageID

--- pkg/fake/fake.go
+++ pkg/fake/fake.go
@@ -12,7 +12,7 @@ import (
 
 // PageID is the ID of our example page.
 // It can be viewed here: https://ancient-gibbon-2cd.notion.site/Example-Page-96245c8f178444a482ad1941127c3ec3
-const PageID notion.Id = "96245c8f178444a482ad1941127c3ec3"
+const PageID notion.Id = "96245c8f-1784-44a4-82ad-1941127c3ec3"
 
 // Responses contains a number of responses we have generated.
 //
